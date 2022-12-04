553f1dfb52651c929235ba0b705dc908c195be3f
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2022-12-04 07:17:13 -0600

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// import env
+use std::env;
+
+// Starting to test rust
 fn main() {
-    println!("Hello, world!");
+    // reading command line arguments
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    println!("Me gusta Rust");
 }
