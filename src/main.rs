2f4f3bb7268d563dbc16e89e92badd2adce06011
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2022-12-11 23:47:59 -0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ use std::env;
 fn main() {
     // reading command line arguments
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    println!("{:?} - How this works?", args);
     println!("Me gusta Rust");
+    if args.len() > 1 {
+        println!("Hello, {}!", args[1]);
+    } else {
+        println!("Hello, World!");
+    }
 }
